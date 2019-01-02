c20ffac563ee2586603a4fe09d7ea05914b4e180
aswink1266 <aswin@labglo.com>
2019-01-02 09:58:15 +0530

    chapter6

--- polls/models.py
+++ polls/models.py
@@ -13,6 +13,10 @@ class Question(models.Model):
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -21,3 +25,6 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
+
+
--- polls/views.py
+++ polls/views.py
@@ -4,6 +4,7 @@ from django.template import loader
 from django.urls import reverse
 from .models import Question, Choice
 from django.views import generic
+from django.utils import timezone
 
 
 # def index(request):
@@ -44,6 +45,12 @@ class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
 
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
 
 # def results(request, question_id):
 #     question = get_object_or_404(Question, pk=question_id)
@@ -70,3 +77,13 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
+def get_queryset(self):
+    """
+    Return the last five published questions (not including those set to be
+    published in the future).
+    """
+    return Question.objects.filter(
+        pub_date__lte=timezone.now()
+    ).order_by('-pub_date')[:5]
